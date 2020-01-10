6275e734ae2242ca12701835b8d8d42abcd3fc7c
vladkudino89 <vlad.kudinov.89@gmail.com>
2020-01-10 08:05:10 +0300

    Move Model User to Entities.

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Entities;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -16,7 +16,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+//        'name',
+        'email', 'password',
     ];
 
     /**
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,9 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
-use App\User;
+/** @var Factory $factory */
+
+use App\Entities\User;
+use Illuminate\Database\Eloquent\Factory;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -18,7 +20,7 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+//        'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password

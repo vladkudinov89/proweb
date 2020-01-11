c21fd98c674c81f1ab2b88692f0175024c5405ea
vladkudino89 <vlad.kudinov.89@gmail.com>
2020-01-11 17:30:07 +0300

    Add users seeder.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,7 +29,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
-//        'role' => $active ? $faker->randomElement([User::ROLE_USER, User::ROLE_ADMIN]) : User::ROLE_USER,
+        'role' => $active ? $faker->randomElement([User::ROLE_USER, User::ROLE_ADMIN]) : User::ROLE_USER,
         'gender' => $active ? User::STATUS_MALE : User::STATUS_FEMALE,
     ];
 });
